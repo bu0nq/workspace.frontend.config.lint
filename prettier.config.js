33ab7e2f8997039f4d6e4cb416aefe9910c95eea
bu0nq <67270989+bu0nq@users.noreply.github.com>
2025-09-29 02:00:36 +0300

    Update

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/configuration
+ */
+export default {
+    semi: false,
+    singleQuote: true,
+    printWidth: 80,
+    trailingComma: 'es5',
+}
